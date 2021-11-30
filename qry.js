bf80ea06252837e5dc4e1e572e685586d6f007ad
shaan shaik <shakeebshaan@gmail.com>
2021-11-30 19:21:28 +0530

    as

--- qry.js
+++ qry.js
@@ -3,3 +3,9 @@ $(window).on("load resize ", function() {
   var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
   $('.tbl-header').css({'padding-right':scrollWidth});
 }).resize();
+
+$(document).ready(function () {
+		$('.menu-btn').click(function(event) {
+			$('.navbar-demo').toggleClass('open-nav');
+		});
+	});
